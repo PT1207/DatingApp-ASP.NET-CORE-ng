ed1e625d7de836928ef9d4084514cfcbcc4b47c3
PT1207 <petrutagarcea@gmail.com>
2021-01-02 17:02:28 +0100

    End of section 4 PT

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,19 +3,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
 
-
-
-    [ApiController]
-    [Route("api/[controller]")]
-
-
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -26,6 +21,7 @@ namespace API.Controllers
 
         //api/users
         [HttpGet]
+        [AllowAnonymous]
         public async Task< ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
@@ -34,6 +30,7 @@ namespace API.Controllers
 
 
         //api/user/3
+        [Authorize]
          [HttpGet("{id}")]
         public async Task< ActionResult<AppUser>> GetUser(int id)
         {
---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,9 @@
+namespace API.DTOs
+{
+    public class UserDto
+    {
+        public object Token { get; set; }
+        
+        public string Username { get; set; }
+    }
+}
