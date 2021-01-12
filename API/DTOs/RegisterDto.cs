ead3641631e1e9582a353ebecf95ef18b7d38f1e
PT1207 <petrutagarcea@gmail.com>
2021-01-12 12:55:52 +0100

    End of section 7 PT

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -6,7 +6,10 @@ namespace API.DTOs
     {
         [Required]
         public string Username { get; set; }
+         
+         
          [Required]
+         [StringLength(8, MinimumLength= 4)]
         public string Password { get; set; }
     
     }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -26,9 +26,6 @@ login(){
   this.accountService.login(this.model).subscribe(response =>{
  this.router.navigateByUrl("/members");
 
-  },error => {
-    console.log(error);
-    this.toastr.error(error.error);
   })
 }
 
